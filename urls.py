83dbdc5505bb575531851fc4838ca713b9daf1e0
unknown <dklein@.riotgames.com>
2011-12-13 12:46:10 +0000

    Changed urls to no longer require serversonfire django-side. On devadmin-new it is required by the apache conf

--- urls.py
+++ urls.py
@@ -4,16 +4,16 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^serversonfire/$', 'serversonfire.sof_main.views.home'),
-	url(r'^serversonfire/ajax/$', 'serversonfire.sof_main.views.ajax'),
+    url(r'^$', 'serversonfire.sof_main.views.home'),
+	url(r'^ajax/$', 'serversonfire.sof_main.views.ajax'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^serversonfire/takeedit/$', 'serversonfire.sof_main.views.takeedit'),
-	url(r'^serversonfire/addnewrow/$', 'serversonfire.sof_main.views.newrow'),
-	url(r'^serversonfire/delete/$', 'serversonfire.sof_main.views.deleterow'),
-	url(r'^serversonfire/changecat/$', 'serversonfire.sof_main.views.changecat'),
+	url(r'^takeedit/$', 'serversonfire.sof_main.views.takeedit'),
+	url(r'^addnewrow/$', 'serversonfire.sof_main.views.newrow'),
+	url(r'^delete/$', 'serversonfire.sof_main.views.deleterow'),
+	url(r'^changecat/$', 'serversonfire.sof_main.views.changecat'),
 )
 
 urlpatterns = urlpatterns + patterns('',
-	(r'^serversonfire/stuff/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'c:\code\SoF\serversonfire\stuff'}),
+	(r'^stuff/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/var/www/html/sof/serversonfire/stuff'}),
 )
